1a3f3e6d03a01919fe59904027df230d7a208e69
NoLag-id <sfpher6@gmail.com>
2025-10-22 10:37:45 +0800

    Update LoaderV1.lua

--- LoaderV1.lua
+++ LoaderV1.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://paste.debian.net/plainh/cf4e4242/", true))()
+loadstring(game:HttpGet("https://paste.debian.net/plainh/4267289e/", true))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/azbalalaff/loading-screen/refs/heads/main/loader.lua"))()
